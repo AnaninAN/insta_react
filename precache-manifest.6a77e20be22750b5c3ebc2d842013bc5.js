f0efd4c6a677b02ce9e4ce4202ac98684e539cef
AnaninAN <ananinpgu@mail.ru>
2019-05-29 12:07:31 +0300

    Updates

---
+++ precache-manifest.6a77e20be22750b5c3ebc2d842013bc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3277cc4de923a9cbf59519101bce5013",
+    "url": "/my-rep/index.html"
+  },
+  {
+    "revision": "7fd9fdbcb9f08bf6c465",
+    "url": "/my-rep/static/css/main.ceee6f9a.chunk.css"
+  },
+  {
+    "revision": "1ed8e819f477e0504d72",
+    "url": "/my-rep/static/js/2.9878c672.chunk.js"
+  },
+  {
+    "revision": "7fd9fdbcb9f08bf6c465",
+    "url": "/my-rep/static/js/main.8ff83b14.chunk.js"
+  },
+  {
+    "revision": "ff293eeb0701fdfdb29b",
+    "url": "/my-rep/static/js/runtime~main.edd783f0.js"
+  },
+  {
+    "revision": "3b8ad90a7a5366149b0595e667396d65",
+    "url": "/my-rep/static/media/error.3b8ad90a.png"
+  },
+  {
+    "revision": "5a3dabc8d25da5978f1bdbc77515645f",
+    "url": "/my-rep/static/media/loading.5a3dabc8.svg"
+  },
+  {
+    "revision": "9e8a2b3937952b3de7aca04cb0d74020",
+    "url": "/my-rep/static/media/logo.9e8a2b39.svg"
+  }
+]);
